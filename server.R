732fb65ced2f2fc3e1eef46ec9f3cb1faed8b919
Darienzhangyy <yz273@duke.edu>
2015-11-12 22:01:22 -0500

    Same result as the blogger

--- server.R
+++ server.R
@@ -12,14 +12,25 @@ shinyServer(
 
     priors = reactive(
       {
+
+        #About n_socks, the number of socks in Karl Broman’s laundry
+        #must be positive and discrete.
+        #It is reasonable to choose a Poisson distribution as a prior
+        #since it is used widely for "count" data.
+        #Furthermore, we can use the negative binomial prior.
         d_n_sock = integer()
         if (input$total_prior == "pois")
         {
           d_n_sock = rpois(input$n_sims, input$total_lambda)
         } else {
-          d_n_sock = rnbinom(input$n_sims,size = input$total_r, prob = input$total_p)
+          size1 = -input$total_mu^2 / (input$total_mu - input$total_sd^2)
+          d_n_sock = rnbinom(input$n_sims,mu = input$total_mu, size = size1)
         }
 
+        #Instead of putting priors on n_paired and the resulting n_odd, we can put prior on
+        #the proportion of paired socks.
+        #Since proportion should be bigger than 0 and smaller than 1, we can use beta distribution
+        #and truncated normal distribution (0 to 1)
         d_prop_pair = numeric()
         if (input$prop_prior == "beta")
         {
@@ -28,26 +39,24 @@ shinyServer(
           d_prop_pair = rtruncnorm(input$n_sims,0,1,input$prop_mu,input$prop_sigma)
         }
 
+        #With the prior on proportion, we can easily calculate the priors for n_pair
+        #and n_odd. Thus we can get four prior distributions where these two are what
+        #we most care about. By simulation afterward and picking out the rows with same
+        #number of pairs as the user input, we can pickout the rows with these four parameters
+        #and get the posterior distribution based on the picked rows.
         d_pair = integer()
-        if (input$prop_prior == "beta")
-        {
         d_pair = round(floor(d_n_sock / 2)*d_prop_pair)
-        }else{
-        d_pair = round(floor(d_n_sock / 2)*d_prop_pair)
-        }
 
         d_odd = integer()
-        if (input$prop_prior == "beta")
-        {
         d_odd = d_n_sock - d_pair *2
-        }else{
-        d_odd = d_n_sock - d_pair *2
-        }
 
         data.frame(total = d_n_sock, prop = d_prop_pair, n_pair = d_pair, n_odd = d_odd)
       }
     )
 
+    #The purpose of simulation is to create a column of simulated number of pairs,
+    #which can be used to be compared with user-input "n_paired" to get the rows
+    #for getting the posterior distribution
     sims = reactive (
       {
         gen_model = function(prior_n_socks,prior_prop_pairs, prior_pair, prior_odd)
@@ -70,13 +79,16 @@ shinyServer(
 
           return(result)
         }
+
+        #Creating a column of simulated number of pairs so as to be used later to
+        #pick out the rows for posterior distribution
         apply(priors(),1, function(x) gen_model(x[1],x[2],x[3],x[4]))
       }
    )
 
     #pick out the rows from the dataframe result from prior() where "pairs" "variable"
     #equals the number of pairs user input, aka, n_paired
-    #Later on the data frame drawn like this (with the same second column values)
+    #Later on the data frame drawn like this (with the same "pair" values)
     #is used to get the posterior distrbution for the four variables
     posterior = reactive(
       {
@@ -91,30 +103,57 @@ shinyServer(
         par(mfrow=c(1,4),mar=c(15,3,4,1))
         #Prior for n_sock
         hist(priors()[,1],  main="Prior on n_socks",col="green",xlab="")
+        abline(v = median(priors()[,1]), lty = 2, col = "red", lwd=3)
         #Prior for prop_pairs
         hist(priors()[,2],  main="Prior on prop_pairs",col="green",xlab="")
+        abline(v = median(priors()[,2]), lty = 2, col = "red", lwd=3)
         #Prior for n_pairs
         hist(priors()[,3],  main="Resulting prior on n_pairs",col="green",xlab="")
+        abline(v = median(priors()[,3]), lty = 2, col = "red", lwd=3)
         #Prior for n_odd
         hist(priors()[,4], main="Resulting prior on n_odd",col="green",xlab="")
+        abline(v = median(priors()[,4]), lty = 2, col = "red", lwd=3)
       }
     )
 
 
     output$all_posterior = renderPlot(
       {
+        #Check the number of selected rows that construct the posterior distribution
         cat("Post: ",dim(posterior()),"\n")
 
         par(mfrow=c(1,4),mar=c(15,3,4,1))
         #posterior for n_sock
         hist(posterior()[,1],  main="Posteior on n_socks",col="blue",xlab="")
+        abline(v = median(posterior()[,1]), lty = 2, col = "red",lwd=3)
         #posterior for prop_pairs
         hist(posterior()[,2], main="Posterior on prop_pairs",col="blue",xlab="")
+        abline(v = median(posterior()[,2]), lty = 2, col = "red",lwd=3)
         #posterior for n_pairs
         hist(posterior()[,3], main="Posterior on n_pairs",col="blue",xlab="")
+        abline(v = median(posterior()[,3]), lty = 2, col = "red",lwd=3)
         #posterior for n_odd
         hist(posterior()[,4], main="Posterior on n_odd",col="blue",xlab="")
+        abline(v = median(posterior()[,4]), lty = 2, col = "red",lwd=3)
       }
     )
+
+
+    output$postTable = renderTable(
+      {
+          dataFrame = data.frame("Medium" = c(round(median(posterior()[,1])),
+                                              median(posterior()[,2]),
+                                              round(median(posterior()[,3])),
+                                              round(median(posterior()[,4]))))
+          rownames(dataFrame) = c("Number of Socks", "Proportion of Pairs", "Number of Pairs",
+                                  "Number of Odd")
+          dataFrame
+      }
+    )
+
+    output$text1 <- renderText({paste("You have get", input$n_total,"socks.")})
+    output$text2 <- renderText({paste(input$n_paired,"of them are paired.")})
+    output$text3 <- renderText({paste("You can predict that there are",
+                                round(median(posterior()[,1])), "socks in all.")})
   }
 )
--- ui.R
+++ ui.R
@@ -1,19 +1,19 @@
 library(shiny)
 
-total_priors = c("Poisson" = "pois", "Negative Binomial" = "nbinom")
+total_priors = c("Negative Binomial" = "nbinom", "Poisson" = "pois")
 prop_priors = c("Beta" = "beta", "Truncated Normal"="tnorm")
 
 shinyUI(
   fluidPage(
     titlePanel(
-      "Socks ABC"
+      "Socks"
     ),
     sidebarPanel(
-      numericInput("n_sims", h4("Simulations:"),value = 3000, min = 100, step = 1),
+      numericInput("n_sims", h4("Simulations:"),value = 100000, min = 100, step = 1),
       hr(),
       h4("Data:"),
       sliderInput("n_total", "Total Socks:", min=0, max=30, value=11),
-      sliderInput("n_paired", "Paired Socks:", min=0, max=30, value=1),
+      sliderInput("n_paired", "Paired Socks:", min=0, max=30, value=0),
       hr(),
       h4("Priors:"),
       selectInput("total_prior", "Prior for total", total_priors),
@@ -21,18 +21,18 @@ shinyUI(
       hr(),
       h4("Hyperparameters:"),
       conditionalPanel(
+        condition="input.total_prior == 'nbinom'",
+        numericInput("total_mu",HTML("Total prior - mu"), value = 30, min=1, max=120),
+        numericInput("total_sd",HTML("Total prior - sd"), value = 15, min=0, max=1)
+      ),
+      conditionalPanel(
         condition="input.total_prior == 'pois'",
         sliderInput("total_lambda",HTML("Total prior - &lambda;"), value = 5, min=1, max=120)
       ),
       conditionalPanel(
-        condition="input.total_prior == 'nbinom'",
-        numericInput("total_r",HTML("Total prior - r"), value = 5, min=1, max=120),
-        numericInput("total_p",HTML("Total prior - p"), value = 0.5, min=0, max=1)
-      ),
-      conditionalPanel(
         condition="input.prop_prior == 'beta'",
-        numericInput("prop_alpha",HTML("Total prior - &alpha;"), value = 1/3 , min=0, max=NA),
-        numericInput("prop_beta",HTML("Total prior - &beta;"), value = 1, min=0, max=NA)
+        numericInput("prop_alpha",HTML("Total prior - &alpha;"), value = 15 , min=0, max=NA),
+        numericInput("prop_beta",HTML("Total prior - &beta;"), value = 2, min=0, max=NA)
       ),
       conditionalPanel(
         condition="input.prop_prior == 'tnorm'",
@@ -45,7 +45,13 @@ shinyUI(
       plotOutput("all_prior"),
       br(),
       h4("Posteriors:"),
-      plotOutput("all_posterior")
+      plotOutput("all_posterior"),
+      br(),
+      h4("Posterior Statistics:"),
+      tableOutput("postTable"),
+      textOutput("text1"),
+      textOutput("text2"),
+      textOutput("text3")
     )
   )
 )
